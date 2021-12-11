5422396b415dee64b314b9b4fac5847418f5927a
Jared Leonguerrero <linuxpir8@gmail.com>
2021-12-10 22:45:07 -0600

    edited placement of console.log

--- level_1/fizzbuzz/index.js
+++ level_1/fizzbuzz/index.js
@@ -1,5 +1,4 @@
 for (i = 1; i < 100; i++) {
-    console.log(i);
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
     } else if (i % 3 === 0) {
@@ -7,4 +6,5 @@ for (i = 1; i < 100; i++) {
     } else if (i % 5 === 0) {
         console.log("Buzz");
     }
+    console.log(i);
 }
